27e3f36cc457761e9e8bb58d1a63680f45ace3d8
xilinch@126.com <a4641146>
2019-06-20 18:46:01 +0800

    调整网络请求

--- app/src/main/java/com/shishic/cb/fragment/MainFragment4.java
+++ app/src/main/java/com/shishic/cb/fragment/MainFragment4.java
@@ -57,6 +57,8 @@ public class MainFragment4 extends BaseFragment {
 
     private ViewPager viewPager;
 
+    private ImageAdapter adapter;
+
     private ArrayList<ImageView> imageViews = new ArrayList<>();
 
     private List<ADTextBean> adTextBeans = new ArrayList<>();
@@ -83,7 +85,7 @@ public class MainFragment4 extends BaseFragment {
 
     private ViewSwitcher.ViewFactory viewFactory;
 
-    private TextView tv_1,tv_2,tv_3,tv_4,tv_5,tv_6,tv_7,tv_8;
+    private TextView tv_1, tv_2, tv_3, tv_4, tv_5, tv_6, tv_7, tv_8;
 
     private Handler handler = new Handler() {
         @Override
@@ -114,89 +116,90 @@ public class MainFragment4 extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if(view == null){
-            view = inflater.inflate(R.layout.fragment_main4,container,false);
-        }
-        textSwitcher = view.findViewById(R.id.textSwitcher);
-        tv_ad = view.findViewById(R.id.tv_ad);
-        viewPager = view.findViewById(R.id.viewPager);
-        tv_1 = view.findViewById(R.id.tv_1);
-        tv_2 = view.findViewById(R.id.tv_2);
-        tv_3 = view.findViewById(R.id.tv_3);
-        tv_4 = view.findViewById(R.id.tv_4);
-        tv_5 = view.findViewById(R.id.tv_5);
-        tv_6 = view.findViewById(R.id.tv_6);
-        tv_7 = view.findViewById(R.id.tv_7);
-        tv_8 = view.findViewById(R.id.tv_8);
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_main4, container, false);
+            textSwitcher = view.findViewById(R.id.textSwitcher);
+            tv_ad = view.findViewById(R.id.tv_ad);
+            viewPager = view.findViewById(R.id.viewPager);
+            tv_1 = view.findViewById(R.id.tv_1);
+            tv_2 = view.findViewById(R.id.tv_2);
+            tv_3 = view.findViewById(R.id.tv_3);
+            tv_4 = view.findViewById(R.id.tv_4);
+            tv_5 = view.findViewById(R.id.tv_5);
+            tv_6 = view.findViewById(R.id.tv_6);
+            tv_7 = view.findViewById(R.id.tv_7);
+            tv_8 = view.findViewById(R.id.tv_8);
 
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                switch (v.getId()) {
-                    case R.id.tv_1:
-                        intent.setClass(getContext(), TrendNumberActivity.class);
-                        break;
-                    case R.id.tv_2:
-                        intent.setClass(getContext(), LostAnalyActivity.class);
-                        break;
-                    case R.id.tv_3:
-                        intent.setClass(getContext(), HistoryActivity.class);
-                        break;
-                    case R.id.tv_4:
-                        intent.setClass(getContext(), NumberChoiceActivity.class);
-                        break;
-                    case R.id.tv_5:
-                        intent.setClass(getContext(), DoubleToolActivity.class);
-                        break;
-                    case R.id.tv_6:
-                        intent.setClass(getContext(), NumberChoiceFilterActivity.class);
-                        break;
-                    case R.id.tv_7:
-                        intent.setClass(getContext(), HistoryActivity.class);
-                        break;
-                    case R.id.tv_8:
-                        intent.setClass(getContext(), SpecialActivity.class);
-                        break;
+            View.OnClickListener onClickListener = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent();
+                    switch (v.getId()) {
+                        case R.id.tv_1:
+                            intent.setClass(getContext(), TrendNumberActivity.class);
+                            break;
+                        case R.id.tv_2:
+                            intent.setClass(getContext(), LostAnalyActivity.class);
+                            break;
+                        case R.id.tv_3:
+                            intent.setClass(getContext(), HistoryActivity.class);
+                            break;
+                        case R.id.tv_4:
+                            intent.setClass(getContext(), NumberChoiceActivity.class);
+                            break;
+                        case R.id.tv_5:
+                            intent.setClass(getContext(), DoubleToolActivity.class);
+                            break;
+                        case R.id.tv_6:
+                            intent.setClass(getContext(), NumberChoiceFilterActivity.class);
+                            break;
+                        case R.id.tv_7:
+                            intent.setClass(getContext(), HistoryActivity.class);
+                            break;
+                        case R.id.tv_8:
+                            intent.setClass(getContext(), SpecialActivity.class);
+                            break;
+                    }
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    getContext().startActivity(intent);
                 }
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                getContext().startActivity(intent);
-            }
-        };
-        tv_1.setOnClickListener(onClickListener);
-        tv_2.setOnClickListener(onClickListener);
-        tv_3.setOnClickListener(onClickListener);
-        tv_4.setOnClickListener(onClickListener);
-        tv_5.setOnClickListener(onClickListener);
-        tv_6.setOnClickListener(onClickListener);
-        tv_7.setOnClickListener(onClickListener);
-        tv_8.setOnClickListener(onClickListener);
-        initRecycleView();
-        initSwitcher();
-        requestNotice();
-        initViewPager();
+            };
+            tv_1.setOnClickListener(onClickListener);
+            tv_2.setOnClickListener(onClickListener);
+            tv_3.setOnClickListener(onClickListener);
+            tv_4.setOnClickListener(onClickListener);
+            tv_5.setOnClickListener(onClickListener);
+            tv_6.setOnClickListener(onClickListener);
+            tv_7.setOnClickListener(onClickListener);
+            tv_8.setOnClickListener(onClickListener);
+            initRecycleView();
+            initSwitcher();
+            requestNotice();
+            initViewPager();
+        }
+
         return view;
     }
 
     /**
      *
      */
-    private void initViewPager(){
-        for(int i = 0; i< 3; i++){
-            ImageView imageView = new ImageView(getContext());
-//            Glide.with(getContext()).load(newsIcon[i]).centerCrop().placeholder(R.mipmap.banner1).into(imageView);
-            Glide.with(getContext()).load(R.mipmap.banner1).centerCrop().placeholder(R.mipmap.banner1).into(imageView);
-            imageViews.add(imageView);
-        }
-        ImageAdapter adapter = new ImageAdapter();
+    private void initViewPager() {
+//        for(int i = 0; i< 3; i++){
+//            ImageView imageView = new ImageView(getContext());
+////            Glide.with(getContext()).load(newsIcon[i]).centerCrop().placeholder(R.mipmap.banner1).into(imageView);
+//            Glide.with(getContext()).load(R.mipmap.banner1).centerCrop().placeholder(R.mipmap.banner1).into(imageView);
+//            imageViews.add(imageView);
+//        }
+        adapter = new ImageAdapter();
         viewPager.setAdapter(adapter);
     }
 
-    private void initRecycleView(){
+    private void initRecycleView() {
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         List<NewsBean> list = new ArrayList<>();
-        newsAdapter = new NewsAdapter(list,getActivity());
+        newsAdapter = new NewsAdapter(list, getActivity());
         recyclerView.setAdapter(newsAdapter);
         requestNews();
 
@@ -206,26 +209,32 @@ public class MainFragment4 extends BaseFragment {
     /**
      * 获取新闻数据
      */
-    private void requestNews(){
+    private void requestNews() {
         //
         RequestUtil.httpPost(getContext(), Constant.URL_NEWS_LIST, null, new NFHttpResponseListener<String>() {
             @Override
             public void onErrorResponse(LogError logError) {
-                LogUtil.e("my","URL_NEWS_LIST error:" );
+                LogUtil.e("my", "URL_NEWS_LIST error:");
             }
 
             @Override
             public void onResponse(String o) {
                 //拿到新闻数据显示内容
-                try{
+                try {
                     JSONObject jsonObject = new JSONObject(o);
                     JSONObject data = jsonObject.optJSONObject("data");
                     JSONArray listJSONArray = data.getJSONArray("list");
-                    List<NewsBean> list = new Gson().fromJson(listJSONArray.toString(),new TypeToken<List<NewsBean>>(){}.getType());
-                    LogUtil.e("my","URL_NEWS_LIST:" + o + " list:" + list);
-                    newsAdapter.changeDate(list);
-                } catch (Exception exception){
-                   exception.printStackTrace();
+                    if(listJSONArray != null){
+                        List<NewsBean> list = new Gson().fromJson(listJSONArray.toString(), new TypeToken<List<NewsBean>>() {
+                        }.getType());
+                        LogUtil.e("my", "URL_NEWS_LIST:" + o + " list:" + list);
+                        newsAdapter.changeDate(list);
+                    }
+
+
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                    LogUtil.e("my","");
                 }
 
             }
@@ -234,8 +243,8 @@ public class MainFragment4 extends BaseFragment {
     }
 
 
-    private void initSwitcher(){
-        adTextBeans.add(new ADTextBean("欢迎光临",""));
+    private void initSwitcher() {
+        adTextBeans.add(new ADTextBean("欢迎光临", ""));
         newMessage(true);
     }
 
@@ -259,7 +268,7 @@ public class MainFragment4 extends BaseFragment {
                     textView.setTextSize(14);//字号
                     textView.setTextColor(getResources().getColor(R.color.c_gray_666666));
 //                    textView.setEllipsize(TextUtils.TruncateAt.END);
-                    textView.setPadding(DensityUtils.dipTopx(getContext(),10),0,0,0);
+                    textView.setPadding(DensityUtils.dipTopx(getContext(), 10), 0, 0, 0);
                     FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
                     params.gravity = Gravity.CENTER_VERTICAL;
                     textView.setLayoutParams(params);
@@ -286,7 +295,7 @@ public class MainFragment4 extends BaseFragment {
         }
     }
 
-    public void requestNotice(){
+    public void requestNotice() {
         RequestUtil.httpGet(getContext(), Constant.URL_NOTICE_LIST, new HashMap<String, String>(), new NFHttpResponseListener<String>() {
             @Override
             public void onErrorResponse(LogError error) {
@@ -295,23 +304,30 @@ public class MainFragment4 extends BaseFragment {
 
             @Override
             public void onResponse(String response) {
-                LogUtil.e("my","URL_NOTICE_LIST response:" + response);
+                LogUtil.e("my", "URL_NOTICE_LIST response:" + response);
                 try {
                     JSONObject jsonObject = new JSONObject(response);
                     JSONObject data = jsonObject.optJSONObject("data");
                     JSONArray jsonArray = data.optJSONArray("list");
-                    if(jsonArray != null && jsonArray.length() > 0){
-                        adTextBeans = new Gson().fromJson(jsonArray.toString(), new TypeToken<List<ADTextBean>>(){}.getType());
+                    if (jsonArray != null && jsonArray.length() > 0) {
+                        adTextBeans = new Gson().fromJson(jsonArray.toString(), new TypeToken<List<ADTextBean>>() {
+                        }.getType());
                         //组装一次所有的内容
                         StringBuilder sb = new StringBuilder();
                         int size = adTextBeans.size();
-                        for(int i = 0; i < size ;i++){
+                        for (int i = 0; i < size; i++) {
                             sb.append("                 ").append(adTextBeans.get(i).getTitle()).append("           ");
+                            ImageView imageView = new ImageView(getContext());
+                            Glide.with(getContext()).load(adTextBeans.get(i).getContent()).centerCrop().placeholder(R.mipmap.banner1).into(imageView);
+                            imageViews.add(imageView);
                         }
                         tv_ad.setText(sb.toString());
+                        if(adapter != null){
+                            adapter.notifyDataSetChanged();
+                        }
                     }
                     newMessage(true);
-                } catch (Exception exception){
+                } catch (Exception exception) {
                     exception.printStackTrace();
                 } finally {
 
@@ -320,7 +336,7 @@ public class MainFragment4 extends BaseFragment {
         });
     }
 
-    private class ImageAdapter extends PagerAdapter{
+    private class ImageAdapter extends PagerAdapter {
 
         public ImageAdapter() {
             super();
@@ -328,13 +344,13 @@ public class MainFragment4 extends BaseFragment {
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,DensityUtils.dipTopx(getContext(),120));
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, DensityUtils.dipTopx(getContext(), 120));
 //            params.width = DensityUtils.getScreenWidth(getContext());
 //            params.height = DensityUtils.dipTopx(getContext(),120);
             container.removeView(imageViews.get(position % imageViews.size()));
-            container.addView(imageViews.get(position % imageViews.size()), 0,params);
+            container.addView(imageViews.get(position % imageViews.size()), 0, params);
             ImageView imageView = imageViews.get(position % imageViews.size());
-            if(adTextBeans != null && adTextBeans.size() > 0){
+            if (adTextBeans != null && adTextBeans.size() > 0) {
                 int setAd = position % (adTextBeans.size());
                 ADTextBean adTextBean = adTextBeans.get(setAd);
                 Glide.with(getContext()).load(adTextBean.getContent()).centerCrop().placeholder(R.mipmap.banner1).into(imageView);
